e643380d274ebba5fed5abea2a06757fb417463e
Rastilan <christoperbhall@gmail.com>
2022-05-16 12:20:40 -0400

    home js done

--- src/home.js
+++ src/home.js
@@ -1,4 +1,8 @@
 window.Home = function() {
+
+    if(document.getElementById("rest-name")){
+        return;
+    }
     
     //Set Root of Div
     const rootDiv = document.getElementById("content");
@@ -49,10 +53,53 @@ window.Home = function() {
     centerDiv.setAttribute("id", "center");
     innercontainerDiv.insertBefore(centerDiv, innercontainerDiv.children[1]);
 
+    // INNER CONTENT
+    //rest name
+    let restNameDiv = document.createElement("div");
+    restNameDiv.setAttribute("id", "rest-name");
+    restNameDiv.innerHTML = "Café Leblanc";
+    centerDiv.insertBefore(restNameDiv, centerDiv.children[0]);
+
+    //rest quote
+    let restQuoteDiv = document.createElement("div");
+    restQuoteDiv.setAttribute("id", "rest-quote");
+    restQuoteDiv.innerHTML = "Leblanc is a café in Yongen-Jaya owned by Sojiro Sakura, famous for its signature coffee and curry.";
+    centerDiv.insertBefore(restQuoteDiv, centerDiv.children[1]);
+
+    //rest hours container
+    let restHoursContainerDiv = document.createElement("div");
+    restHoursContainerDiv.setAttribute("id", "rest-hours-container");
+    centerDiv.insertBefore(restHoursContainerDiv, centerDiv.children[2]);
+
+    //rest hours title
+    let restHoursTitleDiv = document.createElement("div");
+    restHoursTitleDiv.setAttribute("id", "rest-hours-title");
+    restHoursTitleDiv.innerHTML = "Hours";
+    restHoursContainerDiv.insertBefore(restHoursTitleDiv, restHoursContainerDiv.children[0]);
+
+    //rest hours
+    let restHoursDiv = document.createElement("div");
+    restHoursDiv.setAttribute("id", "rest-hours");
+    restHoursDiv.innerHTML = "Sunday: 8am - 8pm      <br/> Monday: 6am - 6pm      <br/>Tuesday: 6am - 6pm     <br/>Wednesday: 6am - 6pm   <br/>Thursday: 6am - 10pm   <br/>Friday: 6am - 10pm     <br/> Saturday: 8am - 10pm";
+    restHoursContainerDiv.insertBefore(restHoursDiv, restHoursContainerDiv.children[1]);
+
+    let locationDiv = document.createElement("div");
+    locationDiv.setAttribute("id", "location");
+    locationDiv.innerHTML = "Yongen-Jaya";
+    centerDiv.insertBefore(locationDiv, centerDiv.children[3]);
+
+
     //right div creation
     let rightDiv = document.createElement("div");
     rightDiv.setAttribute("id", "right");
     innercontainerDiv.insertBefore(rightDiv, innercontainerDiv.children[2]);
+
+
+    //footer div creation
+    let footerDiv = document.createElement("div");
+    footerDiv.setAttribute("id", "footer");
+    footerDiv.innerHTML = "Webpage built using hopes and dreams... but mostly Javascript - This project is just a proof of concept for the code. The design is very basic due to this.";
+    rootDiv.insertBefore(footerDiv, rootDiv.children[2]);
   
 
 }
